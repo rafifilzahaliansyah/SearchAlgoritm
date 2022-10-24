486e8f3e9191f6dc406f37266d325aa84be75650
Rafi Filzah Aliansyah <rafi.filzah.ft21@mail.umy.ac.id>
2022-10-24 02:45:28 -0700

    menambahkan loop pada pencarian

--- SearchAlgoritm/Program.cs
+++ SearchAlgoritm/Program.cs
@@ -54,6 +54,27 @@ namespace SearchAlgoritm
                 int upperbound = n-1
 
                 //obtain the index of the middle elements
+                int mid = (lowerbound + upperbound) / 2;
+                int ctr = 1;
+
+                //loop to search for the elements in the array
+                while ((item! = arr[mid]) && (lowerbound <= upperbound))
+                {
+                    if (item > arr[mid])
+                        lowerbound = mid + 1;
+                    else
+                        upperbound = mid - 1;
+
+                    mid = (lowerbound + upperbound) / 2;
+                    ctr++;
+                }
+                if (item == arr[mid])
+                    Console.WriteLine("\n" + item.ToString() + "found at position" + (mid + 1));
+                else
+                    Console.WriteLine("\n" + item.ToString() + "not found in the array\n");
+                Console.WriteLine("\nNumber of comparasion : " + ctr);
+
+                Console.Write("\ncontinue search (y/n) : ");
             }
         }
         static void Main(string[] args)
