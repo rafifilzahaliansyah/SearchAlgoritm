5f8a38fe81e04da87996568972253d88b9be8b8d
Rafi Filzah Aliansyah <rafi.filzah.ft21@mail.umy.ac.id>
2022-10-24 02:25:43 -0700

    menambahkan banyaknya element array

--- SearchAlgoritm/Program.cs
+++ SearchAlgoritm/Program.cs
@@ -19,9 +19,24 @@ namespace SearchAlgoritm
         {
             while (true)
             {
-
-
-                UriFormat==if 
+                Console.Write("enter the number of elements in the array : ");
+                string s = Console.ReadLine();
+                n = Int32.Parse(s);
+                if ((n > 0) && (n <= 20))
+                    break;
+                else
+                    Console.WriteLine("array should have minimum 1 and maximum 20 elemets.");
+            }
+            //accept array elements
+            Console.WriteLine("");
+            Console.WriteLine("=======================");
+            Console.WriteLine("enter array elements : ");
+            Console.WriteLine("=======================");
+            for (i=0;i<n;i++)
+            {
+                Console.Write("<" + (i + 1) + ">");
+                string s1 = Console.ReadLine();
+                arr[i] = Int32.Parse(s1);
             }
         }
         static void Main(string[] args)
