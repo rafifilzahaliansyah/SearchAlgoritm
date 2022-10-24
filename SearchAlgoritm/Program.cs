7208d422bdaac0a1c8c309f5b78b6028c5b274d5
Asus <Asus@DESKTOP-P7MVRS7>
2022-10-24 02:11:06 -0700

    Add project files.

---
+++ SearchAlgoritm/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SearchAlgoritm
+{
+    class Program
+    {
+        //Array to be searched
+        int[] arr = new int[20];
+        //number of elements in the array
+        int n;
+        //get the number of elements to store in the array
+        int i;
+
+        public void input()
+        {
+            while (true)
+            {
+
+            }
+        }
+        static void Main(string[] args)
+        {
+        }
+    }
+}
