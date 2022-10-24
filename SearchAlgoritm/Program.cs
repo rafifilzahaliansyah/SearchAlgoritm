8178dbe1f7b6da0b623c028f3bd28c1a736f8135
Rafi Filzah Aliansyah <rafi.filzah.ft21@mail.umy.ac.id>
2022-10-24 02:55:26 -0700

    menambhakan Linear Search

--- SearchAlgoritm/Program.cs
+++ SearchAlgoritm/Program.cs
@@ -51,7 +51,7 @@ namespace SearchAlgoritm
 
                 //apply binary search
                 int lowerbound = 0;
-                int upperbound = n-1
+                int upperbound = n - 1;
 
                 //obtain the index of the middle elements
                 int mid = (lowerbound + upperbound) / 2;
@@ -75,6 +75,26 @@ namespace SearchAlgoritm
                 Console.WriteLine("\nNumber of comparasion : " + ctr);
 
                 Console.Write("\ncontinue search (y/n) : ");
+                ch = char.Parse(Console.ReadLine());
+            } while ((ch == 'y') || (ch == 'y'));
+        }
+
+        public void LinearSearch()
+        {
+            char ch;
+            //search for number of comparison
+            int ctr;
+            do
+            {
+                //accept the number to be searched
+                Console.Write("\nenter elements want you to search : ");
+                int item = Convert.ToInt32(Console.ReadLine());
+
+                ctr = 0;
+                for (i = 0; i<n;i++)
+                {
+                    ctr++
+                }
             }
         }
         static void Main(string[] args)
